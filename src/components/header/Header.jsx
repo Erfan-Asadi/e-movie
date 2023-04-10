3fd0855a503c632ce9db9a3e1bfe010df11e44cb
Erfan Asadi <asadi.love7981@gmail.com>
2023-04-10 11:30:12 +0430

    developing Header(2:26, untill importing style.scss content)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './style.scss'
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import "./style.scss";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
 
 
 const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
-    <div>Header</div>
+    <header className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigate("")}>Movies</li>
+          <li className="menuItem">TV Show</li>
+          <li className="menuItem"><HiOutlineSearch /></li>
+        </ul>
+      </ContentWrapper>
+    </header>
   )
 }
 
