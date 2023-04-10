1173ebe61ae3ceed9b4d6056698c5c2c60d22203
Erfan Asadi <asadi.love7981@gmail.com>
2023-04-11 02:57:50 +0430

    switchingTabs logic(3:16)

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './style.scss';
+import { useState } from 'react';
+
+const SwitchTabs = ({ data, onTabChange }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const activeTab = (tab, index) => {
+    // because width's of each tab is 100px
+    setLeft(100 * index);
+    setTimeout(() => {
+      setSelectedTab(index);
+    }, 300);
+    onTabChange(tab, index);
+  }
+  return (
+    <div className='switchingTabs'>
+      <div className="tabItems">
+        {data.map((tab, index) => (
+          <span key={index} className={`tabItem ${selectedTab === index ? "active" : ""}`} onClick={() => activeTab(tab, index)}>{tab}</span>
+        ))}
+        <span className='movingBg' style={{ left: left }}></span>
+      </div>
+
+    </div>
+  )
+}
+
+export default SwitchTabs
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import './style.scss';
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+
+const Trending = () => {
+
+    const onTabChange = (tab) => {
+
+    }
+
+    return (
+        <div className='carouselSection'>
+            <ContentWrapper>
+                <span className="carouselTitle">Trending</span>
+                <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+            </ContentWrapper>
+        </div>
+    )
+}
+
+export default Trending
