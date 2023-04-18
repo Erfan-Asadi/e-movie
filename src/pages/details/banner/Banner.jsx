9edd1fa74b5652c3c6d81275d477a3f249fe1fc6
Erfan Asadi <asadi.love7981@gmail.com>
2023-04-18 14:24:57 +0430

    develop Banner.jsx untill Genres(4:46)

--- src/pages/details/banner/Banner.jsx
+++ src/pages/details/banner/Banner.jsx
@@ -10,27 +10,55 @@ import useFetch from "../../../hooks/useFetch";
 import Genres from "../../../components/genres/Genres";
 import Img from "../../../components/laziLoadImage/Img";
 import CircularRating from '../../../components/circularRating/CircularRating';
-// import PosterFallback from "../../../assets/no-poster.png";
+import PosterFallback from "../../../assets/no-poster.png";
 // import { PlayIcon } from "../Playbtn";
 // import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const Banner = ({ video, crew, loading, data }) => {
+
+    const { url } = useSelector(state => state.home);
+    const _genres = data?.genres?.map(g => g.id);
 
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor((totalMinutes / 60));
         const minutes = (totalMinutes % 60);
         return `${hours}h:${minutes < 10 ? `0${minutes}` : `${minutes}`}m`
     }
-    const { url } = useSelector(state => state.home);
 
     return (
         <div className="detailsBanner">
             {!loading ? (
                 <>
                     {!!data && (
+                        <>
                         <div className="backdrop-img">
                             <Img src={url.backdrop + data.backdrop_path} />
                         </div>
+                        <div className="opacity-layer"></div>
+                        <ContentWrapper>
+                            <div className="content">
+                                <div className="left">
+                                    {data.poster_path ? (
+                                        <Img className="posterImg"
+                                             src={url.backdrop + data.poster_path} />
+                                             ) : (
+                                        <Img className="posterImg"
+                                             src={PosterFallback} />
+                                    )}
+                                </div>
+                                <div className="right">
+                                    <div className="title">
+                                        {`${data.name || data.title} (${dayjs(data.release_data).format('YYYY')})`}
+                                    </div>
+                                    <div className="subtitle">
+                                        {data.tagline}
+                                    </div>
+                                    <Genres data={_genres} />
+                                </div>
+                            </div>
+                        </ContentWrapper>
+                        </>
+                        
                     )
                     }
                 </>
