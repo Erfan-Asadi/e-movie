2624695e38ae56c980c9038ee52d0a8572bb13cd
Erfan Asadi <asadi.love7981@gmail.com>
2023-04-23 11:58:38 +0430

    add NotFound page

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ContentWrapper from '../../components/contentWrapper/ContentWrapper'
 import './style.scss'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
   )
 }
 
